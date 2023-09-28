601fd5dd6e4da961f5968cc6c356a8546f6463d2
Ольга Мелихова <melihova.o@yandex.ru>
2023-09-28 20:47:37 +0300

    feat: SubCategoryInline

--- backend/online_store/products/admin.py
+++ backend/online_store/products/admin.py
@@ -5,12 +5,19 @@ from django.contrib import admin
 from products.models import Category, SubCategory
 
 
+class SubCategoryInline(admin.TabularInline):
+    """Subcategories widget on the Category object creation page."""
+
+    model = SubCategory
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     """Representation of the Category model in the admin panel."""
 
     list_display = ("id", "name", "slug", "image")
     search_fields = ("name",)
+    inlines = (SubCategoryInline,)
 
 
 @admin.register(SubCategory)
